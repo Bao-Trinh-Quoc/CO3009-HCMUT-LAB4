edb6f1a4af090fbb6238b965cbce3826f8f5dfa3
Cloudy <bao.trinhquoc@hcmut.edu.vn>
2024-11-14 21:55:17 +0700

    Implemented SCH_Init and SCH_Update

--- Core/Src/scheduler.c
+++ Core/Src/scheduler.c
@@ -5,4 +5,35 @@
  *      Author: DELL
  */
 
+#include "scheduler.h"
 
+SCH_Task tasks[SCH_TASKNUMBER];
+
+void SCH_Init(void) {
+	for (uint8_t i = 0; i < SCH_TASKNUMBER; i++) {
+		tasks[i].functionPointer = 0;
+		tasks[i].id = SCH_TASKNUMBER - i - 1;
+		tasks[i].delay = 0;
+		tasks[i].period = 0;
+		tasks[i].flag = 0;
+	}
+}
+
+void SCH_Update(void) {
+	if (tasks[0].functionPointer == 0)
+		return;
+
+	if (tasks[0].delay > 0) {
+		if (tasks[0].delay > SCH_TIMERTICK)
+			tasks[0].delay -= SCH_TIMERTICK;
+		else
+			tasks[0].delay = 0;
+	}
+
+	if (tasks[0].delay == 0)
+		tasks[0].flag = 1;
+}
+
+void SCH_Dispatch(void) {}
+uint8_t SCH_AddTask(void (*functionPointer)(void), uint32_t delay, uint32_t period) {}
+unsigned char SCH_DeleteTask(uint8_t id) {}
